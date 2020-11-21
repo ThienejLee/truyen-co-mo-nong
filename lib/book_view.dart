463ba5f20a6252e81e8fa6a3f4204c789c64905a
ThienejLee <thienvanlea1@gmail.com>
2020-11-21 23:56:50 +0700

    done logic bookview

--- lib/book_view.dart
+++ lib/book_view.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BookView extends StatefulWidget {
@@ -7,9 +10,27 @@ class BookView extends StatefulWidget {
   _BookViewState createState() => _BookViewState();
 }
 
+Firestore database = Firestore.instance;
+
 class _BookViewState extends State<BookView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: Text("Truyện")),
+        body: FutureBuilder(
+            future: database.collection("MyBook").document(widget.bookId).get(),
+            builder: (context, dataSnapshot) {
+              if (!dataSnapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              return Column(
+                children: <Widget>[
+                  Text(dataSnapshot.data["title"],),
+                  Text(dataSnapshot.data["description"],),
+                  Text(dataSnapshot.data["author"],),
+
+                ],
+              );
+            }));
   }
 }
