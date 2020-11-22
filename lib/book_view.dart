b6477f058da15220c09724a203201edd82dcae4b
ThienejLee <thienvanlea1@gmail.com>
2020-11-22 22:07:16 +0700

    test pdf views

--- lib/book_view.dart
+++ lib/book_view.dart
@@ -1,7 +1,7 @@
-import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 
 class BookView extends StatefulWidget {
   final String bookId;
@@ -12,25 +12,41 @@ class BookView extends StatefulWidget {
 
 Firestore database = Firestore.instance;
 
+
+
+
 class _BookViewState extends State<BookView> {
+  String url = "http://www.pdf995.com/samples/pdf.pdf";
+  String pdfasset = "assets/sample.pdf";
+  PDFDocument _doc;
+  bool _loading;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPdf();
+  }
+
+  _initPdf() async {
+    setState(() {
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromAsset("assets/sample.pdf");
+    setState(() {
+      _doc=doc;
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Truyện")),
-        body: FutureBuilder(
-            future: database.collection("MyBook").document(widget.bookId).get(),
-            builder: (context, dataSnapshot) {
-              if (!dataSnapshot.hasData) {
-                return CircularProgressIndicator();
-              }
-              return Column(
-                children: <Widget>[
-                  Text(dataSnapshot.data["title"],),
-                  Text(dataSnapshot.data["description"],),
-                  Text(dataSnapshot.data["author"],),
-
-                ],
-              );
-            }));
+      appBar: AppBar(title: Text("Flutter PDF Demo"),),
+      body: _loading ? Center(child: CircularProgressIndicator(),) : PDFViewer(document: _doc,
+        indicatorBackground: Colors.red,
+        // showIndicator: false,
+        // showPicker: false,
+      ),
+    );
   }
 }
